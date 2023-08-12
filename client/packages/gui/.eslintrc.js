dc730014bddd8bd4d60739e9532e187392b0f19d
Sayafdine Said <musikid@outlook.com>
2023-08-12 23:56:49 +0200

    feat(client/gui): init

---
+++ client/packages/gui/.eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  extends: ["plugin:svelte/recommended", "plugin:prettier"],
+  rules: {},
+};
---
+++ client/packages/gui/src/App.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<main class="container">
+  <h1>Welcome to Tauri!</h1>
+</main>
