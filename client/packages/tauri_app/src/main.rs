50646c025db1fbe10d2c091ffb534e2e128b3d45
santarl <rfsjay@gmail.com>
2025-02-15 19:06:17 +0400

    Client: add build script for gui
    Client gui is now in a buildable state

--- client/packages/tauri_app/src/main.rs
+++ client/packages/tauri_app/src/main.rs
@@ -1,13 +1,6 @@
-use tauri::{command, generate_handler, Builder};
-
-#[command]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
-}
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    Builder::default()
-        .invoke_handler(generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    tauri_app_lib::run()
 }
